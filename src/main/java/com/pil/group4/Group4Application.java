b84b3a8a6dad112f4f94a802d19e9f496f782644
Fer Noceti <fer.j.noceti@gmail.com>
2022-12-29 12:27:16 -0300

    Update Group4Application.java

--- src/main/java/com/pil/group4/Group4Application.java
+++ src/main/java/com/pil/group4/Group4Application.java
@@ -2,8 +2,9 @@ package com.pil.group4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class Group4Application {
 
 	public static void main(String[] args) {
