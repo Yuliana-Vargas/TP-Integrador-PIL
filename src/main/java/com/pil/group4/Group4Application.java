4b1ac1f4b5a32a9032f0a16d0f9ef80583f89e68
Fer Noceti <fer.j.noceti@gmail.com>
2022-12-29 12:08:19 -0300

    initial commit

---
+++ src/main/java/com/pil/group4/Group4Application.java
@@ -0,0 +1,13 @@
+package com.pil.group4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pil/group4/Group4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pil.group4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
