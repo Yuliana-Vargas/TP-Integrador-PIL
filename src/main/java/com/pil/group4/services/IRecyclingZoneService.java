1a0032654a6c0fd8cdb7ffd88d9eb445dfecdd25
Yuliana Vargas <yulii.vargas@gmail.com>
2023-01-10 15:39:36 -0300

    Interface IRecyclingZoneService added

---
+++ src/main/java/com/pil/group4/services/IRecyclingZoneService.java
@@ -0,0 +1,15 @@
+package com.pil.group4.services;
+
+import com.pil.group4.models.RecyclingZoneModel;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+public interface IRecyclingZoneService {
+    ArrayList<RecyclingZoneModel> getRecyclingZones();
+    Optional<RecyclingZoneModel> getRecyclingZoneById(Long idRecyclingZone);
+    RecyclingZoneModel saveRecyclingZone(RecyclingZoneModel recyclingZoneModel);
+    boolean deleteOfRecyclingZone(Long idRecyclingZone);
+    RecyclingZoneModel updateRecyclingZoneById(Long idRecyclingZone, RecyclingZoneModel recyclingZoneModel);
+
+}
