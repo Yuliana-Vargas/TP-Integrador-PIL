9f5af430585d73b77f0fa1bc06bc764644fa9c7e
MartinMaciel110916 <martinmacielbramoso@gmail.com>
2023-01-10 15:40:25 -0300

    Enum ClassificationType

--- src/main/java/com/pil/group4/models/RecyclingZoneModel.java
+++ src/main/java/com/pil/group4/models/RecyclingZoneModel.java
@@ -20,6 +20,9 @@ public class RecyclingZoneModel {
     private StateOfTheZone stateOfTheZone;
 
     @Column
+    private ClassificationType classificationType;
+
+    @Column
     private boolean needsReclassification;
 
     /*
@@ -63,6 +66,10 @@ public class RecyclingZoneModel {
     public void setStateOfTheZone(StateOfTheZone stateOfTheZone) {
         this.stateOfTheZone = stateOfTheZone;
     }
+
+    public ClassificationType getClassificationType(){ return classificationType; }
+
+    public void setClassificationType(ClassificationType classificationType){ this.classificationType = classificationType; }
 
     public void setNeedsReclassification(boolean needsReclassification) {
         this.needsReclassification = needsReclassification;
