b12ad9d719421d9a845a36c259ebcac6e8619d76
Yuliana Vargas <yulii.vargas@gmail.com>
2023-01-11 21:50:01 -0300

    SupervisorController added

---
+++ src/main/java/com/pil/group4/services/SupervisorService.java
@@ -0,0 +1,15 @@
+package com.pil.group4.services;
+
+import com.pil.group4.repositories.SupervisorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SupervisorService {
+    private final SupervisorRepository supervisorRepository;
+
+    @Autowired
+    public SupervisorService(SupervisorRepository supervisorRepository) {
+        this.supervisorRepository = supervisorRepository;
+    }
+}
